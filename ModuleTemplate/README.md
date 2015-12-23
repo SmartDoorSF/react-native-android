7a817facb7ce1b24e8f59d9d10061842b57effc7
Ian Zhang <ianzhang1986@gmail.com>
2015-12-22 22:46:39 -0800

    [Bugfix] Add facebook.react dependency in /src/build.gradle

---
+++ ModuleTemplate/README.md
@@ -0,0 +1,8 @@
+App build gradle file in 
+
+projectName/android/app/build.gradle
+
+dependencies:
+
+Add:
+  compile 'com.facebook.react:react-native:0.17+'
--- ModuleTemplate/android/app/src/main/java/com/smartdoorsf/nativemodule/firstmodule/MainActivity.java
+++ ModuleTemplate/android/app/src/main/java/com/smartdoorsf/nativemodule/firstmodule/MainActivity.java
@@ -41,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "TodoLite";
 
+    private ReactInstanceManager mReactInstanceManager;
+    private ReactRootView mReactRootView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
